f6693bc6fa3b6afcd53a000e71c6dc673e572609
alptuu <alptugbozkurt2004@gmail.com>
2026-01-04 19:09:30 +0300

    emailHostPassword Hidden

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -177,5 +177,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'alptugbozkurt2004@gmail.com'  
-EMAIL_HOST_PASSWORD = 'nzjxxvihlpldkffz'  # No spaces!
+###EMAIL_HOST_PASSWORD = 
 DEFAULT_FROM_EMAIL = 'alptugbozkurt2004@gmail.com'
